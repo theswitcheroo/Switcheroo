eefab791da5a6a626053c63526f4623176f13504
Nick Neuman <nick.m.neuman@gmail.com>
2017-11-27 22:42:01 -0600

    Function for creating an ID- truct mapping. Function for Purchase contract factory.

--- safe-purchase/solidity-contracts/contracts/Purchase.sol
+++ safe-purchase/solidity-contracts/contracts/Purchase.sol
@@ -1,4 +1,5 @@
 pragma solidity ^0.4.18;
+import "Purchase_Factory.sol";
 
 // To do
 // Delivery confirmation function - DONE
--- safe-purchase/solidity-contracts/contracts/Purchase_Factory.sol
+++ safe-purchase/solidity-contracts/contracts/Purchase_Factory.sol
@@ -1,4 +1,5 @@
-import "Purchase.sol"
+pragma solidity ^0.4.18;
+import "Purchase.sol";
 
 //----------------------------------------------------------------
 //FACTORY CONTRACT - Master contract that creates individual purchase contracts
@@ -10,34 +11,24 @@ contract PurchaseCreator {
     struct PurchaseData {
         address buyer;
         address seller;
-        uint value;
+        uint price;
     }
 
     //Mapping Transaction ID's to each transaction so we can easily track later
     uint public nextPurchaseId;
-    mapping(uint => PurchaseData) public purchases; //should we use uint256 instead of uint?
+    mapping(uint => PurchaseData) public purchases;
 
-    function newPurchase (address buyer, address seller, uint value) returns (uint PurchaseId) {
-        //Below attempts to create a struct for the new Purchase contract with
-        //info that can be easily looked up using the PurchaseId
-        PurchaseData storage purchase = purchases[nextPurchaseId];
-        purchase.buyer = buyer;
-        purchase.seller = seller;
-        purchase.value = value;
-        nextPurchaseId ++;
-        PurchaseId = nextPurchaseId;
+    //Creates new struct value & ties it to a PurchaseId key in the mapping
+    function newPurchase(address buyer, address seller, uint price) returns (uint PurchaseId) {
+        PurchaseId = nextPurchaseId++;
+        purchases[PurchaseId] = PurchaseData(buyer, seller, price);
 
-        return new Purchase(); //attempt to create new instance of Purchase contract, but not correct
         return PurchaseId;
     }
 
-    //This is an external getter function to access the data from outside
-    //the contract, I think it can be used to check status from web3
-    //not sure if useful...
-
-    /*function getPurchaseData(PurchaseData) constant {
-        var id = purchases[PurchaseId];
-        return (purchases(PurchaseData)[id].buyer, purchases(PurchaseData)[id].seller, purchases(PurchaseData)[id].value);
-    }*/
-
+    //Creates new Purchase child contract with the PurchaseId
+    //Can use PurchaseId in the child contract to pull in PurchaseData struct
+    function newPurchase(uint PurchaseId) payable {
+        address newPurch = new Purchase(PurchaseId);
+    }
 }
